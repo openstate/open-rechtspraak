ec35c26c3e2795c1e3d52c2e509712c59cc1ebe2
jonakemon <jonathan@seib.nl>
2021-02-06 18:36:22 +0100

    Update error page

--- app/errors.py
+++ app/errors.py
@@ -2,12 +2,12 @@ from flask import render_template
 
 
 def unauthorized_error(e):
-    return render_template("error.html"), 401
+    return render_template("pages/error.html"), 401
 
 
 def page_not_found(e):
-    return render_template("error.html"), 404
+    return render_template("pages/error.html"), 404
 
 
 def internal_server_error(e):
-    return render_template("error.html"), 500
+    return render_template("pages/error.html"), 500
