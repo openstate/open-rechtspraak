968fec170b78b9aadbc32a3cfd4e3bbb95dfdaad
jonakemon <jonathan@seib.nl>
2021-02-06 18:14:50 +0100

    Center person name

--- app/assets/scripts/templates.js
+++ app/assets/scripts/templates.js
@@ -3,7 +3,7 @@ export const personTemplate = (person) => {
         <a class="search-result-person" href="person/${person.id}">
           <div class="card mb-3">
             <div class="card-body">
-              <h3>${person.toon_naam}</h3>
+              <h3 class="search-result-person__name">${person.toon_naam}</h3>
             </div>
           </div>
         </a>
