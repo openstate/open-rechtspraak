0125c75d3ab89d749eba8524ec74abf1b171e5df
jonakemon <jonathan@seib.nl>
2021-02-06 15:02:42 +0100

    Further style in OSF format, refactor partials

--- app/routes.py
+++ app/routes.py
@@ -8,8 +8,12 @@ base_bp = Blueprint("base", __name__)
 @base_bp.route("/")
 def index():
     People.query.all()
-    return render_template("index.html")
+    return render_template("pages/index.html")
+
 
 @base_bp.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("pages/about.html")
+
+
+redirect_bp = Blueprint("redirect", __name__)
