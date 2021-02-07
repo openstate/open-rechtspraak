4b4ae1aad7f19752ef41ef85bec0131fb5af85a8
jonakemon <jonathan@seib.nl>
2021-02-07 17:50:03 +0100

    Add limit / offset for search endpoint

--- app/routes_api.py
+++ app/routes_api.py
@@ -8,12 +8,24 @@ api_bp = Blueprint("api", __name__, url_prefix="/api/v1")
 @api_bp.route("/search")
 def search():
     q = request.args.get("q", None)
+    limit = request.args.get("limit", default=20, type=int)
+
+    if limit > 100:
+        limit = 20
+
+    offset = request.args.get("offset", type=int)
     query = People.query.filter(People.protected.isnot(True))
 
     if q:
         query = query.filter(People.toon_naam.ilike(f"%{q}%"))
 
     return jsonify(
-        data=[person.serialize for person in query.limit(100).all()],
+        data=[
+            person.serialize
+            for person in query.order_by(People.last_name.asc())
+            .offset(offset)
+            .limit(limit)
+            .all()
+        ],
         count=query.count(),
     )
