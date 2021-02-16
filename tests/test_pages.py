94eea46af7735428e34cac8a5d12d8749c3099dd
jonakemon <jonathan@seib.nl>
2021-02-17 00:10:48 +0100

    Add tests for sitemap

--- tests/test_pages.py
+++ tests/test_pages.py
@@ -1,3 +1,6 @@
+from tests.factories import PersonFactory
+
+
 def test_index(client):
     r = client.get("/")
     assert r.status_code == 200
@@ -6,3 +9,20 @@ def test_index(client):
 def test_about(client):
     r = client.get("/about")
     assert r.status_code == 200
+
+
+def test_sitemap(client):
+    r = client.get("/sitemap.xml")
+    assert r.status_code == 200
+
+
+def test_unprotected_person_in_sitemap(client):
+    person = PersonFactory().save()
+    r = client.get("/sitemap.xml")
+    assert str(person.id) in r.get_data(as_text=True)
+
+
+def test_protected_person_not_in_sitemap(client):
+    person = PersonFactory(protected=True).save()
+    r = client.get("/sitemap.xml")
+    assert str(person.id) not in r.get_data(as_text=True)
