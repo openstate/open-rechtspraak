8b24a481fc386aeac6761e951e369f9dcc0043ed
Sicco van Sas <siccovansas@users.noreply.github.com>
2021-08-24 11:59:49 +0200

    Automatically import db and models when you start a Flask shell

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 from app import app
+from app.extensions import db
+from app.models import (
+    PersonVerdict, Person, ProfessionalDetail, SideJob, Verdict, Institution,
+    ProcedureType, LegalArea
+)
+
+# This piece of code automatically imports the db and models when you start a
+# Flask shell
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db,
+        'PersonVerdict': PersonVerdict,
+        'Person': Person,
+        'ProfessionalDetail': ProfessionalDetail,
+        'SideJob': SideJob,
+        'Verdict': Verdict,
+        'Institution': Institution,
+        'ProcedureType': ProcedureType,
+        'LegalArea': LegalArea
+    }
 
 if __name__ == "__main__":
     app.run(port=5000, host="0.0.0.0")
