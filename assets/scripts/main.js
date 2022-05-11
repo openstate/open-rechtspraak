3ac194ebd6958429b096119cc438dfa2784a20ba
Jonakemon <jonathan@seib.nl>
2022-05-11 20:23:55 +0200

    Add tooltip to recente uitspraken

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,2 +1,7 @@
-import 'bootstrap/dist/js/bootstrap.bundle';
+import { Tooltip } from 'bootstrap';
 import './search';
+
+const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new Tooltip(tooltipTriggerEl)
+})
