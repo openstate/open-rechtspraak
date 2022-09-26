7576ca3c4169c640734f7c8dd6c0f165f7d65f66
Jonakemon <jonathan@seib.nl>
2022-09-26 15:10:00 +0200

    Remove final native js

--- assets/scripts/main.ts
+++ assets/scripts/main.ts
@@ -1,8 +1,3 @@
-import { Tooltip } from 'bootstrap';
+import './onLoad';
 import './search';
 import './donation';
-
-const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new Tooltip(tooltipTriggerEl)
-})
