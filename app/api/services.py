cb166d17c65ec889df8b048a33798e010e2a30c8
kemon <jonathan@seib.nl>
2023-05-21 21:06:30 +0200

    Fixes ordering bug described in issue #34

--- app/api/services.py
+++ app/api/services.py
@@ -13,11 +13,28 @@ class BaseService:
             query_params.get("limit", default=self.DEFAULT_LIMIT, type=int)
         )
         self.offset = query_params.get("offset", default=0, type=int)
+        self.queryset = None
+        self.order: list = []
 
     def _max_limit(self, limit: int):
         if limit > self.MAX_LIMIT:
             limit = self.MAX_LIMIT
         return limit
+
+    def apply_ordering(self):
+        if len(self.order) == 0:
+            raise ValueError(
+                "You did not specify any attributes to order the queryset on."
+            )
+
+        for order in self.order:
+            self.queryset = self.queryset.order_by(order)
+        return self.queryset
+
+    def apply_pagination(self):
+        self.queryset = self.queryset.limit(self.limit)
+        self.queryset = self.queryset.offset(self.offset)
+        return self.queryset
 
 
 class PersonService(BaseService):
@@ -25,7 +42,7 @@ class PersonService(BaseService):
         super().__init__(query_params)
         self.query_params = query_params
         self.queryset = Person.query.filter(Person.protected.isnot(True))
-        self.order = Person.last_name.asc()
+        self.order = [Person.last_name.asc(), Person.id.asc()]
 
     def list_query(self, query_params):
         q = query_params.get("q", None)
@@ -40,7 +57,7 @@ class PersonService(BaseService):
                 Person.removed_from_rechtspraak_at.is_(None)
             )
 
-        self.queryset = self.queryset.order_by(self.order)
+        self.queryset = self.apply_ordering()
         self.queryset = self.queryset.limit(self.limit)
         self.queryset = self.queryset.offset(self.offset)
         return self.queryset
@@ -57,10 +74,10 @@ class PersonVerdictsService(BaseService):
             .options(joinedload(Verdict.procedure_type))
             .filter(PersonVerdict.person_id == person.id)
         )
-        self.order = Verdict.issued.desc()
+        self.order = [Verdict.issued.desc(), Verdict.id.asc()]
 
     def list_query(self):
-        self.queryset = self.queryset.order_by(self.order)
+        self.queryset = self.apply_ordering()
         self.queryset = self.queryset.limit(self.limit)
         self.queryset = self.queryset.offset(self.offset)
         return self.queryset
