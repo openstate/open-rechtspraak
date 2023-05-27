efd442bd43c8250747dcb1b16503e7a8ad5e4d13
kemon <jonathan@seib.nl>
2023-05-27 14:42:36 +0200

    Check whether ids are valid uuids before giving them to database

--- app/api/routes.py
+++ app/api/routes.py
@@ -3,6 +3,7 @@ from flask import Blueprint, abort, jsonify, redirect, request, url_for
 from app.api.serializers import person_list_serializer, verdict_serializer
 from app.api.services import PersonService, PersonVerdictsService
 from app.models import Person
+from app.util import is_valid_uuid
 
 api_bp = Blueprint("api", __name__, url_prefix="/api/v1")
 
@@ -31,6 +32,9 @@ def person():
 
 @api_bp.route("/person/<id>/verdicts")
 def person_verdicts(id):
+    if not is_valid_uuid(id):
+        abort(404)
+
     person = Person.query.filter(Person.id == id).first()
 
     if not person or person.protected:
@@ -41,7 +45,7 @@ def person_verdicts(id):
 
     service.apply_ordering()
     service.apply_pagination()
-    verdicts = service.queryset().all()
+    verdicts = service.queryset.all()
 
     return jsonify(
         data=[verdict_serializer(item) for item in verdicts],
