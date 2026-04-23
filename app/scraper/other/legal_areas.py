1265299ed6cf64d3933721a4cae5b23279ad24ee
kemon <jonathan@seib.nl>
2026-04-23 11:56:54 +0200

    Fix deprecated findChildren method

--- app/scraper/other/legal_areas.py
+++ app/scraper/other/legal_areas.py
@@ -26,7 +26,7 @@ def import_legal_areas_handler():
         r = session.get(LEGAL_AREAS_URL)
         r.raise_for_status()
 
-        main_areas = to_soup(r.content).find("Rechtsgebieden").findChildren("Rechtsgebied", recursive=False)
+        main_areas = to_soup(r.content).find("Rechtsgebieden").find_all("Rechtsgebied", recursive=False)
         current_app.logger.info(f"Found {len(main_areas)} main legal areas")
 
         for main_area in main_areas:
