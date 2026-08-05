bef5944436e1512757c8273c45df1bb6b5699279
Sicco van Sas <siccovansas@users.noreply.github.com>
2026-08-05 13:39:29 +0200

    Update social links in footer

--- assets/scripts/onLoad.ts
+++ assets/scripts/onLoad.ts
@@ -1,14 +1,13 @@
 import { config, dom, library } from '@fortawesome/fontawesome-svg-core';
 import {
-  faFacebook, faGithub,
-  faLinkedin, faTwitter,
+  faMastodon, faBluesky, faLinkedin, faGithub,
 } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { Tooltip } from 'bootstrap';
 
 const initFontawesome = () => {
   config.autoAddCss = false;
-  library.add(fas, faTwitter, faLinkedin, faFacebook, faGithub);
+  library.add(fas, faMastodon, faBluesky, faLinkedin, faGithub);
   dom.watch();
 };
 
